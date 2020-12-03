774ce5e5d9483b55fe4118b627960bf16fdd0d5e
Quinn, Richard <richard.quinn@novartis.com>
2020-12-03 15:24:01 +0100

    Various improvements.

--- src/aoc/2020/day03.py
+++ src/aoc/2020/day03.py
@@ -1,34 +1,43 @@
+from collections import namedtuple as nt
+
 import math
 
 
 def load_file(file_name: str):
-    lines = []
-    with open(file_name) as file:
-        for line in file:
+    lines: list = []
+    with open(file_name) as fd:
+        for line in fd:
             lines.append(line.strip())
     return lines
 
 
-def traverse(right=3, down_step=1):
-    trees = 0
-    for i in range(0, length, down_step):
-        location: int = i // down_step * right % width
+def traverse(right_steps=3, down_steps=1):
+    trees: int = 0
+    for i in range(0, height, down_steps):
+        location: int = i // down_steps * right_steps % width
         trees += 1 if fields[i][location] == "#" else 0
     return trees
 
 
 if __name__ == "__main__":
+
+    Rule: nt = nt('Rule', ['right_steps', 'down_steps'])
+
     fields = load_file("day03.txt")
-    length = len(fields)
+    height = len(fields)
     width = len(fields[0])
+    print(f"Data Width: {height}")
+    print(f"Data Height: {width}\n")
 
-    print(f"Field Width: {length}")
-    print(f"Field Height: {width}\n")
     total_trees = []
-    slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    rules = [Rule(right_steps=1, down_steps=1),
+             Rule(right_steps=3, down_steps=1),
+             Rule(right_steps=5, down_steps=1),
+             Rule(right_steps=7, down_steps=1),
+             Rule(right_steps=1, down_steps=2)]
 
-    for slope in slopes:
-        total_trees.append(traverse(slope[0], slope[1]))
+    for rule in rules:
+        total_trees.append(traverse(rule.right_steps, rule.down_steps))
 
     [print(f"Run {x} number of trees: {y}") for x, y in enumerate(total_trees)]
     print(f"\nProduct: {math.prod(total_trees)}")
