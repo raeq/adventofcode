5095d82c46c116523c28235e10f0240ef9718329
Quinn, Richard <richard.quinn@novartis.com>
2020-12-03 22:44:23 +0100

    With timing

---
+++ src/aoc/2020/day04.py
@@ -0,0 +1,19 @@
+def load_file(file_name: str) -> list:
+    with open(file_name, 'r') as fd:
+        return [line.rstrip('\n') for line in fd]
+
+
+def main():
+    global fields
+    global height
+    global width
+
+    fields = load_file("day04.txt")
+    height = len(fields)
+    width = len(fields[0])
+    print(f"Data Width: {height}")
+    print(f"Data Height: {width}\n")
+
+
+if __name__ == "__main__":
+    main()
