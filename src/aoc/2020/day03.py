db346656c4fb703e367563c8c7b1c0482c65a415
Quinn, Richard <richard.quinn@novartis.com>
2020-12-03 13:47:36 +0100

    Completed solution

--- src/aoc/2020/day03.py
+++ src/aoc/2020/day03.py
@@ -9,10 +9,10 @@ def load_file(file_name: str):
     return lines
 
 
-def traverse(all_fields: list, right=3, down_step=1):
+def traverse(right=3, down_step=1):
     trees = 0
     for i in range(0, length, down_step):
-        spot = all_fields[i][i // down_step * right % width]
+        spot = fields[i][i // down_step * right % width]
         if spot == "#":
             trees += 1
     return trees
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
 
     for slope in slopes:
-        total_trees.append(traverse(fields, slope[0], slope[1]))
+        total_trees.append(traverse(slope[0], slope[1]))
 
     [print(f"Run {x} number of trees: {y}") for x, y in enumerate(total_trees)]
     print(f"\nProduct: {math.prod(total_trees)}")
