707aaf6880d66a57797df031b84b5f1730f9e103
Quinn, Richard <richard.quinn@novartis.com>
2020-12-03 22:46:46 +0100

    Shell

--- src/aoc/2020/day04.py
+++ src/aoc/2020/day04.py
@@ -1,14 +1,18 @@
+from aoc.utils.decorator_utils import timing
+
+
 def load_file(file_name: str) -> list:
     with open(file_name, 'r') as fd:
         return [line.rstrip('\n') for line in fd]
 
 
+@timing
 def main():
     global fields
     global height
     global width
 
-    fields = load_file("day04.txt")
+    fields = load_file("day03.txt")
     height = len(fields)
     width = len(fields[0])
     print(f"Data Width: {height}")
