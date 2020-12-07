484aa7859a28a3443909f5f393cf61c46d4a0e22
Quinn, Richard <richard.quinn@novartis.com>
2020-12-07 19:44:38 +0100

    Day 7 solution

--- src/aoc/twenty/day07.py
+++ src/aoc/twenty/day07.py
@@ -1,8 +1,11 @@
 import typing
 from collections import defaultdict
+from functools import lru_cache
 from pprint import pprint
 
 import regex as re
+
+bags: dict = defaultdict(dict)
 
 
 def load_file(file_name: str) -> typing.List[str]:
@@ -11,8 +14,6 @@ def load_file(file_name: str) -> typing.List[str]:
 
 
 def parse_data(line: str) -> dict:
-    bags: dict = defaultdict(dict)
-
     regex = r"^(\b\S*\b\s\b\S*\b) bags"
     matches = re.findall(regex, line, re.IGNORECASE | re.DOTALL)
     for m in matches:
@@ -21,20 +22,47 @@ def parse_data(line: str) -> dict:
             regex = r"(\b\d\b)\s(\b\S*\b\s\b\S*\b)"
             matches = re.findall(regex, line, re.IGNORECASE | re.DOTALL)
             for b in matches:
-                bags[m][b[1]] = b[0]
+                bags[m][b[1]] = int(b[0])
         else:
-            bags[m] = "empty"
+            pass
+            # bags[m] = ""
     return bags
 
 
-def main():
-    mybags: dict = defaultdict(dict)
+@lru_cache()
+def find_all_outer(bag) -> set:
+    outer_bags = set()
 
+    for key_bag in bags:
+        if bag in bags[key_bag]:
+            outer_bags.add(key_bag)
+            outer_bags.update(find_all_outer(key_bag))
+    if outer_bags:
+        return outer_bags
+
+    return {bag}
+
+
+@lru_cache()
+def find_all_inner(bag) -> int:
+    inner_bags = 0
+
+    for value_bag in bags[bag]:
+        quantity = bags[bag][value_bag]
+        inner_bags += quantity
+        inner_bags += quantity * find_all_inner(value_bag)
+
+    return inner_bags
+
+
+def main():
     for d in load_file("day07.txt"):
         parsed = parse_data(d)
-        mybags.update(parsed)
+        bags.update(parsed)
 
-    pprint(mybags)
+    pprint(bags)
+    print(f"Part one answer: {len(find_all_outer('shiny gold'))}")
+    print(f"Part two answer: {find_all_inner('shiny gold')}")
 
 
 if __name__ == "__main__":
