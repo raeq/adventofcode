601d8c2e869040941904ffa162108fe296453468
Richard Quinn <quinn.richard@gmail.com>
2020-12-20 15:07:35 +0100

    Day 20 solution using networkx and ndimage

--- src/aoc/twenty/day20.py
+++ src/aoc/twenty/day20.py
@@ -1,112 +1,94 @@
-import dataclasses
-import typing
-from pprint import pprint
 from collections import defaultdict
+from itertools import product
+from math import prod
+import networkx as nx
+import numpy as np
+from scipy.ndimage import convolve
 
 
-@dataclasses.dataclass
-class Tile:
-    name: str
-    raw: str = dataclasses.field(repr=False)
-    rows: typing.List = dataclasses.field(default_factory=list, repr=False)
-    edges: typing.Set = dataclasses.field(default_factory=list, repr=True)
-
-    def __init__(self, raw_data: str = ""):
-
-        if raw_data:
-            self.raw = raw_data
-            self.rows = raw_data.strip().split('\n')[1:]
-            self.name = raw_data.strip().split('\n')[0]
-            self.edges = self._fill_edges()
-
-    @staticmethod
-    def _translate_edge(edge: str) -> int:
-        retVal: int = 0
-        try:
-            retVal = int(edge.translate(str.maketrans(".#", "01")), 2)
-        except ValueError as e:
-            raise
-        finally:
-            return retVal
-
-    def _fill_edges(self) -> set:
-        edges: set = set()
-        max = len(self.rows) - 1
-
-        edges.add(self._translate_edge(self.rows[0]))
-        edges.add(self._translate_edge(self.rows[max]))
-
-        left: str = ""
-        right: str = ""
-        for r in self.rows:
-            left += r[0]
-            right += r[max]
-
-        edges.add(self._translate_edge(left))
-        edges.add(self._translate_edge(right))
-
-        return edges
-
-    @staticmethod
-    def _flip_horizontal(rows) -> list:
-        matrix: list = []
-        for r in rows:
-            matrix.append(r[::-1])
-        return matrix
-
-    @staticmethod
-    def _flip_vertical(rows) -> list:
-        matrix: list = []
-        for r in rows[::-1]:
-            matrix.append(r)
-        return matrix
-
-    def mirror(self):
-
-        matrix: list = self._flip_vertical(self._flip_horizontal(self.rows))
-
-        t = Tile()
-        t.raw = self.raw
-        t.name = self.name
-        t.rows = matrix
-        t.edges = t._fill_edges()
-        return t
+def borders(tile):
+    for i in (0, (..., -1), -1, (..., 0)):
+        yield min(tile[i].tobytes(), tile[i][::-1].tobytes())
 
 
-def load_file(file_name: str) -> str:
+border_to_tiles = defaultdict(list)  # Build a network of connected tiles from this
+
+
+def part_one(tiles):
+    # Build dict of connections between tiles
+    for n, tile in tiles.items():
+        for border in borders(tile):
+            border_to_tiles[border].append(n)
+
+    return prod(n for n, tile in tiles.items() if sum(len(border_to_tiles[border]) for border in borders(tile)) == 6)
+
+
+def orientations(tile):
+    for _ in range(4):
+        tile = np.rot90(tile)
+        yield tile
+        yield tile.T
+
+
+def part_two():
+    G = nx.Graph(edge for edge in border_to_tiles.values() if len(edge) == 2)
+
+    # Get a corner
+    for node in G:
+        if len(G[node]) == 2:
+            break
+    # Orient the corner
+    for tile in orientations(tiles[node]):
+        if [len(border_to_tiles[border]) for border in borders(tile)] == [1, 2, 2, 1]:
+            break
+
+    grid = {}
+    grid[0, 0] = node, tile
+    # Fill in the rest of the grid by correctly orienting the neighbors
+    for y, x in product(range(12), repeat=2):
+        node, tile = grid[y, x]
+        for neighbor in G[node]:
+            for neighbor_tile in orientations(tiles[neighbor]):
+                if np.array_equal(tile[-1], neighbor_tile[0]):
+                    grid[y + 1, x] = neighbor, neighbor_tile
+                    break
+                if np.array_equal(tile[:, -1], neighbor_tile[:, 0]):
+                    grid[y, x + 1] = neighbor, neighbor_tile
+                    break
+        G.remove_node(node)
+
+    image = np.block([[grid[y, x][1][1: -1, 1: -1] for x in range(12)] for y in range(12)])
+    lochness = np.array([[char == "#" for char in line] for line in
+                         "                  #  \n"
+                         "#    ##    ##    ### \n"
+                         " #  #  #  #  #  #    \n".splitlines()
+                         ])
+    for lochness in orientations(lochness):
+        if count := (convolve(image, lochness, output=int, mode="constant") == lochness.sum()).sum():
+            return image.sum() - count * lochness.sum()
+
+
+def load_file(file_name: str) -> list:
     with open(file_name, 'r') as fd:
         return fd.read()
 
 
-def main():
-    tiles_raw = load_file("day20.txt").split("\n\n")
-    all_tiles: list = []
-    tile_dict: defaultdict = defaultdict(set)
+def parse_raw(raw):
+    tiles = {}
+    for tile in raw.split("\n\n"):
+        it = iter(tile.splitlines())
+        number = int(next(it)[5:-1])
+        tiles[number] = np.array([[char == "#" for char in line] for line in it])
+    return tiles
 
-    for t in tiles_raw:
-        tile1: Tile = Tile(t)
-        all_tiles.append(tile1)
-        tile2: Tile = tile1.mirror()
-        all_tiles.append(tile2)
-        tile_dict[tile1.name] = tile_dict[tile1.name].union(tile1.edges)
-        tile_dict[tile2.name] = tile_dict[tile2.name].union(tile2.edges)
-
-    edge_dict: defaultdict = defaultdict(set)
-
-    for t in all_tiles:
-        for e in t.edges:
-            edge_dict[e].add(t.name)
-
-    subset: list =[]
-    for y in edge_dict.values():
-        if len(y) < 2:
-            subset.append(y)
-
-    pprint(subset)
-
-    # print(f"Part one solution: {valid_messages(rules, messages):>5}")
-    # print(f"Part two solution: {valid_messages(rules, messages, '2'):>5}")
 
 
 if __name__ == '__main__':
-    main()
+
+    raw = load_file("day20.txt")
+    tiles = parse_raw(raw)
+
+    print(f"Part one solution: {part_one(tiles):>5}")
+    print(f"Part two solution: {part_two():>5}")
+
+
