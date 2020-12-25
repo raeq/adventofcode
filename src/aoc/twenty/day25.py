72b167dda31b2c7238a7d52e546d717d7c221cac
Quinn, Richard <richard.quinn@novartis.com>
2020-12-25 10:04:36 +0100

    Day 25 solution

--- src/aoc/twenty/day25.py
+++ src/aoc/twenty/day25.py
@@ -2,12 +2,12 @@ PRIME: int = int(20201227)
 INITIALIZATION_VECTOR: int = int(7)
 
 
-def load_file(file_name: str) -> list:
+def load_file(file_name: str) -> tuple:
     with open(file_name, 'r') as fd:
         return tuple(map(int, fd.read().split('\n')))
 
 
-def get_secret_loop_size(cardkey: int, doorkey: int) -> tuple[int, int]:
+def get_secret_loop_size(cardkey: int, doorkey: int) -> tuple[int]:
     for n in range(1, PRIME):
         if pow(INITIALIZATION_VECTOR, n, PRIME) == cardkey:
             return n, pow(doorkey, n, PRIME)
