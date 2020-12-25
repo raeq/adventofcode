2e3645f6c47b78566d3e207d2eb52acc15fd6649
Richard Quinn <quinn.richard@gmail.com>
2020-12-25 12:42:56 +0100

    Day21 Better Solution

--- src/aoc/twenty/day21.py
+++ src/aoc/twenty/day21.py
@@ -1,18 +1,51 @@
+from functools import reduce
+from collections import defaultdict
+import regex as re
+
+
 def load_file(file_name: str) -> list:
     with open(file_name, 'r') as fd:
+        return fd.read()
 
-        data = [_.strip().replace(")", "").split('(contains') for _ in fd.readlines()]
-        for _ in data:
-            _[0] = set(_[0].strip().split(" "))
-            _[1] = set(_[1].strip().split(","))
 
-        return data
+def parse_raw(raw):
+    ALLERGEN_RE = r"(.+) \(contains (.+)\)"
+    foods = []
+    contaminated = defaultdict(list)
 
+    for ingredients, allergens in re.findall(ALLERGEN_RE, raw):
+        ingredients = set(ingredients.split())
+        foods.append(ingredients)
+        for allergen in allergens.split(", "):
+            contaminated[allergen].append(ingredients)
+    return foods, {k: reduce(set.intersection, v) for k, v in contaminated.items()}
+
+
+
+
+def part_one(foods, allergens):
+    safe = reduce(set.union, foods) - reduce(set.union, allergens.values())
+    return sum(ingredient in safe for food in foods for ingredient in food)
+
+
+def part_two(allergens):
+    stack = list(allergens.items())
+    canonical = []
+    while stack:
+        stack.sort(key=lambda tup: -len(tup[1]))
+        allergen, (ingredient,) = stack.pop()
+        canonical.append((allergen, ingredient))
+        for _, possible in stack:
+            possible.discard(ingredient)
+    return ",".join(ingredient for _, ingredient in sorted(canonical))
 
 
 def main():
-    data = load_file("day21.txt")
-    print(data)
+    print(f"Part one answer: {part_one(foods, allergens)}")
+    print(f"Part two answer {part_two(allergens)}")
 
 if __name__ == '__main__':
+    raw = load_file("day21.txt")
+    foods, allergens = parse_raw(raw)
+
     main()
