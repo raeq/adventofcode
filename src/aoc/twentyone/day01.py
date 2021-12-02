8d75d0237c1ba9b008e6e2ecc091d26a81476ecc
Quinn, Richard <richard.quinn@novartis.com>
2021-12-02 10:49:10 +0100

    Day 1 refactored

--- src/aoc/twentyone/day01.py
+++ src/aoc/twentyone/day01.py
@@ -1,7 +1,7 @@
 with open("day01.txt") as f:
-    depths = [int(x.strip()) for x in f]
+    data = [int(x.strip()) for x in f]
 
-part1 = sum(y > x for x, y in zip(depths, depths[1:]))
-part2 = sum(y > x for x, y in zip(depths, depths[3:]))
+part1 = sum(y > x for x, y in zip(data, data[1:]))
+part2 = sum(y > x for x, y in zip(data, data[3:]))
 
 print(part1, part2, sep="\n")
