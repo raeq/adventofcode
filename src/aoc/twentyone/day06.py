bc791f3edc15042eac34d76cbcd0bacc81df054f
Quinn, Richard <richard.quinn@novartis.com>
2021-12-06 15:17:25 +0100

    Added the Leslie Matrix solution from Reddit https://www.reddit.com/r/adventofcode/comments/r9z49j/2021_day_6_solutions/hnfna50/?utm_source=share&utm_medium=web2x&context=3

--- src/aoc/twentyone/day06.py
+++ src/aoc/twentyone/day06.py
@@ -17,42 +17,27 @@ def calculate(data: list, cycles=80, counter: Counter = None) -> Tuple[int, Coun
     return sum(counter.values()), counter
 
 
-def add_new_generation(fish_ages_yesterday: Counter) -> dict:
-    fish_ages_new_day: dict = {}
+def add_new_generation(fish_counts_yesterday: Counter) -> dict:
+    fish_counts_new_day: dict = {}
 
-    for age in range(6):
-        fish_ages_new_day[age] = fish_ages_yesterday[age + 1]
-    fish_ages_new_day[6] = fish_ages_yesterday[0] + fish_ages_yesterday[7]
-    fish_ages_new_day[7] = fish_ages_yesterday[8]
-    fish_ages_new_day[8] = fish_ages_yesterday[0]
+    fish_counts_new_day[0] = fish_counts_yesterday[1]
+    fish_counts_new_day[1] = fish_counts_yesterday[2]
+    fish_counts_new_day[2] = fish_counts_yesterday[3]
+    fish_counts_new_day[3] = fish_counts_yesterday[4]
+    fish_counts_new_day[4] = fish_counts_yesterday[5]
+    fish_counts_new_day[5] = fish_counts_yesterday[6]
+    fish_counts_new_day[6] = fish_counts_yesterday[0] + fish_counts_yesterday[7]
+    fish_counts_new_day[7] = fish_counts_yesterday[8]
+    fish_counts_new_day[8] = fish_counts_yesterday[0]
 
-    return fish_ages_new_day
+    return fish_counts_yesterday
 
 
 if __name__ == '__main__':
     raw_data = get_data("day06.txt")
 
-    answer, c = calculate(data=raw_data, cycles=80)
-    print(f"Day 6 Part 1: {answer}")
+    # answer, c = calculate(data=raw_data, cycles=80)
+    # print(f"Day 6 Part 1: {answer}")
 
-    answer, c = calculate(data=raw_data, cycles=256 - 80, counter=c)
+    answer, c = calculate(data=raw_data, cycles=10_000_000)
     print(f"Day 6 Part 2: {answer}")
-
-    # https://en.wikipedia.org/wiki/Leslie_matrix
-    import numpy as np
-
-
-    mat = np.array([[0, 1, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 1, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 1, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 1, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 1, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 1, 0, 0],
-                    [1, 0, 0, 0, 0, 0, 0, 1, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0, 1],
-                    [1, 0, 0, 0, 0, 0, 0, 0, 0]], dtype=np.int64)
-
-    part1 = np.linalg.matrix_power(mat, 80).sum(axis=0)
-    part2 = np.linalg.matrix_power(mat, 256).sum(axis=0)
-
-    print(sum(part1[i] + part2[i] * 1j for i in raw_data))
