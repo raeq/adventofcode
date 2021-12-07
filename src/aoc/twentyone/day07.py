8a0de72d10831133caf04d1ed5640179370c4f37
Quinn, Richard <richard.quinn@novartis.com>
2021-12-07 12:06:13 +0100

    Day 7 solution.

--- src/aoc/twentyone/day07.py
+++ src/aoc/twentyone/day07.py
@@ -1,5 +1,16 @@
-def get_mean(pop: list[int]) -> int:
-    return round(sum(pop) / len(pop))
+from math import floor, ceil
+from statistics import mean, median
+
+
+def sum_c(pop: list[int], centroid: int) -> int:
+    def gauss_sum(n: int) -> int:
+        return (n * (n + 1)) // 2
+
+    return int(sum([gauss_sum(abs(x - centroid)) for x in pop]))
+
+
+def sum_d(pop: list[int], centroid: int) -> int:
+    return int(sum([abs(x - centroid) for x in pop]))
 
 
 def get_data(fn) -> list:
@@ -9,11 +20,14 @@ def get_data(fn) -> list:
         return list(map(int, file_contents.split(',')))
 
 
-def part1(data: list[int]) -> int:
-    ...
-
-
 if __name__ == '__main__':
     file_data = get_data("day07.txt")
-    part1(file_data)
-    print(get_mean(file_data))
+
+    print(f"Day 7 Part 1: {sum_d(file_data, (median(file_data)))}")
+
+    # floor or ceil of mean?
+    m = mean(file_data)
+    fl = sum_c(file_data, floor(m))
+    cl = sum_c(file_data, ceil(m))
+
+    print(f"Day 7 Part 1: {fl if fl < cl else cl}")
